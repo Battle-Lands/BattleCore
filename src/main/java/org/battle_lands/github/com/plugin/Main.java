96e93e0d6d9d8c63b6488635e91bfd8062969b8a
Kewilleen <kewilleen@outlook.com>
2020-09-29 23:24:31 -0300

    Resolving Abnormal plugin type, requeries test

---
+++ src/main/java/org/battle_lands/github/com/plugin/Main.java
@@ -0,0 +1,12 @@
+package org.battle_lands.github.com.plugin;
+
+public class Main extends PluginCore {
+
+    public void onPluginEnable() {
+
+    }
+
+    public void onPluginDisable() {
+
+    }
+}
