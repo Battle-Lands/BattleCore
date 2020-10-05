eec7237691799c2ec3fd572051eeddcb55f2af36
Kewilleen <kewilleen@outlook.com>
2020-10-05 15:43:11 -0300

    add progressbar builder

--- src/main/java/com/github/battle_lands/builders/EntityBuilder.java
+++ src/main/java/com/github/battle_lands/builders/EntityBuilder.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import net.minecraft.server.v1_8_R3.EntityLiving;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftLivingEntity;
+import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -17,24 +18,37 @@ import java.util.List;
 public class EntityBuilder {
 
     private final EntityType entityType;
+    private String name;
+    private double maxHealth;
+    private float health;
     private Location location;
     private boolean ai;
     private final List<PotionEffect> potionEffects;
 
-    public EntityBuilder(EntityType entityType) {
+    public EntityBuilder(EntityType entityType, Location location) {
         this.entityType = entityType;
         this.ai = true;
         this.potionEffects = new ArrayList<>();
-    }
-
-    public EntityBuilder(EntityType entityType, int amount) {
-        this.entityType = entityType;
-        this.ai = true;
-        this.potionEffects = new ArrayList<>();
+        this.location = location;
     }
 
     public EntityBuilder ai(boolean ai) {
         this.ai = ai;
+        return this;
+    }
+
+    public EntityBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public EntityBuilder maxHealth(double health) {
+        this.maxHealth = health;
+        return this;
+    }
+
+    public EntityBuilder health(float health) {
+        this.health = health;
         return this;
     }
 
@@ -50,12 +64,21 @@ public class EntityBuilder {
 
     public Entity build() {
         Entity entity = location.getWorld().spawnEntity(location, entityType);
+        if (name != null) {
+            entity.setCustomName(name);
+            entity.setCustomNameVisible(true);
+        }
         if (entity instanceof LivingEntity) {
             EntityLiving entityLiving = ((CraftLivingEntity) entity).getHandle();
             entityLiving.getDataWatcher().watch(15, (byte) (ai ? 0 : 1));
-            for (PotionEffect potionEffect : potionEffects) {
+
+            for (PotionEffect potionEffect : potionEffects)
                 ((CraftLivingEntity) entity).addPotionEffect(potionEffect);
+            if (maxHealth > 0) {
+                Damageable damageable = (Damageable) entity;
+                damageable.setMaxHealth(maxHealth);
             }
+            entityLiving.setHealth(health > 0 ? health : entityLiving.getHealth());
         }
         return entity;
     }
