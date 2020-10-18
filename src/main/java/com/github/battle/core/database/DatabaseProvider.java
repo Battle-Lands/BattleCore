fa449c1867d3817f9558e9ca7387d384e4eb7474
zkingboos <kingboosoficial@gmail.com>
2020-10-18 20:27:50 -0300

    added docs

--- src/main/java/com/github/battle/core/database/DatabaseProvider.java
+++ src/main/java/com/github/battle/core/database/DatabaseProvider.java
@@ -1,7 +1,11 @@
 package com.github.battle.core.database;
 
+import lombok.NonNull;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author Kewilleen Gomes
@@ -26,13 +30,21 @@ public interface DatabaseProvider {
      * @param <T>              type of return object
      * @return result from searched
      */
-    <T> T result(String query, DatabaseFunction<ResultSet, T> databaseFunction, Object... objects);
+    <T> T result(@NonNull String query, DatabaseFunction<ResultSet, T> databaseFunction, Object... objects);
 
     /**
      * Close connection bridge
      */
     void close();
 
+    /**
+     * Execute updates queries with return update state
+     *
+     * @param query   mysql query
+     * @param objects objects to sync with statement
+     * @return state (0 for nothing, 1 for any, -1 for error)
+     */
+    int executeUpdate(@NonNull String query, Object... objects);
 
     /**
      * Use to bridge connection between your plugin and mysql server
@@ -45,7 +57,7 @@ public interface DatabaseProvider {
      * Verify if jdbc has connection bridge
      * You need to use {@link #connect() connect} method at least once;
      *
-     * @return
+     * @return if has any connection
      */
     boolean hasConnection();
 
@@ -53,5 +65,20 @@ public interface DatabaseProvider {
      * @param query   execute query, update or some think like that
      * @param objects array to sync with result set
      */
-    void execute(String query, Object... objects);
+    void execute(@NonNull String query, Object... objects);
+
+    /**
+     * Sync objects with statement
+     *
+     * @param statement instance of statement
+     * @param objects   array of objects to sync within
+     * @return instance of statement
+     * @throws SQLException {@link PreparedStatement#setObject(int, Object) setObject} can throws errors
+     */
+    default PreparedStatement setStatementObjects(PreparedStatement statement, Object[] objects) throws SQLException {
+        for (int index = 0; index < objects.length; index++) {
+            statement.setObject(index + 1, objects[index]);
+        }
+        return statement;
+    }
 }
--- src/main/java/com/github/battle/core/database/requester/MySQLRequester.java
+++ src/main/java/com/github/battle/core/database/requester/MySQLRequester.java
@@ -3,13 +3,13 @@ package com.github.battle.core.database.requester;
 import com.github.battle.core.database.DatabaseCredential;
 import com.github.battle.core.database.DatabaseFunction;
 import com.github.battle.core.database.DatabaseProvider;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 
 import java.sql.*;
 
 @RequiredArgsConstructor
-public class MySQLRequester implements DatabaseProvider {
+public final class MySQLRequester implements DatabaseProvider {
 
     private final static String MYSQL_URI_CONNECTION = "jdbc:mysql://%s:%s/%s" +
       "?useUnicode=true&characterEncoding=utf-8&autoReconnect=true";
@@ -17,22 +17,66 @@ public class MySQLRequester implements DatabaseProvider {
     private final DatabaseCredential credential;
     private Connection connection;
 
-    @SneakyThrows
+    @Override
+    public <T> T result(@NonNull String query, DatabaseFunction<ResultSet, T> databaseFunction, Object... objects) {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            setStatementObjects(statement, objects);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet != null && resultSet.next()
+                  ? databaseFunction.apply(resultSet)
+                  : databaseFunction.apply(null);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void execute(@NonNull String query, Object... objects) {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            setStatementObjects(
+              statement,
+              objects
+            ).execute();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public int executeUpdate(@NonNull String query, Object... objects) {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            return setStatementObjects(
+              statement,
+              objects
+            ).executeUpdate();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
     public MySQLRequester connect() {
-        Class.forName("com.mysql.jdbc.Driver");
-        final String mysqlConnectionUri = String.format(
-          MYSQL_URI_CONNECTION,
-          credential.getHost(),
-          credential.getPort(),
-          credential.getDatabase()
-        );
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            final String mysqlConnectionUri = String.format(
+              MYSQL_URI_CONNECTION,
+              credential.getHost(),
+              credential.getPort(),
+              credential.getDatabase()
+            );
 
-        this.connection = DriverManager.getConnection(
-          mysqlConnectionUri,
-          credential.getUser(),
-          credential.getPassword()
-        );
-
+            this.connection = DriverManager.getConnection(
+              mysqlConnectionUri,
+              credential.getUser(),
+              credential.getPassword()
+            );
+        } catch (SQLException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
         return this;
     }
 
@@ -52,22 +96,6 @@ public class MySQLRequester implements DatabaseProvider {
     }
 
     @Override
-    public <T> T result(String query, DatabaseFunction<ResultSet, T> databaseFunction, Object... objects) {
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            setStatementObjects(statement, objects);
-
-            try (ResultSet resultSet = statement.executeQuery()) {
-                return resultSet != null && resultSet.next()
-                  ? databaseFunction.apply(resultSet)
-                  : databaseFunction.apply(null);
-            }
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
     public void close() {
         try {
             if (connection != null && !connection.isClosed()) {
@@ -76,24 +104,5 @@ public class MySQLRequester implements DatabaseProvider {
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
-    }
-
-    @Override
-    public void execute(String query, Object... objects) {
-        try (PreparedStatement statement = connection.prepareStatement(query)) {
-            setStatementObjects(
-              statement,
-              objects
-            ).execute();
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    public PreparedStatement setStatementObjects(PreparedStatement statement, Object[] objects) throws SQLException {
-        for (int index = 0; index < objects.length; index++) {
-            statement.setObject(index + 1, objects[index]);
-        }
-        return statement;
     }
 }
